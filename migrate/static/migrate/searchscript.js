30c6aa5a4cdd51baeb6849326f9239bc92ce2110
Swaraj Dhumne <swaraj.dhumne@gmail.com>
2017-02-12 07:30:09 -0500

    added lowercase support and fixed enter key

--- migrate/static/migrate/searchscript.js
+++ migrate/static/migrate/searchscript.js
@@ -3,17 +3,30 @@ var countryNameEl = document.getElementById("countryname");
 
 searchBar.onclick = function (e) {
     if (e.target.id == 'countrysearch') {
-        var countryName = countryNameEl.value;
+        var countryName = capitalizeEachWord(countryNameEl.value);
         fetch("/api/visa/?from=" + countryName)
             .then(r => r.json())
             .then(dat => colorCountries(dat.d));
     }
 }
 
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function capitalizeEachWord(string) {
+    return string
+        .split(' ')
+        .map(capitalizeFirstLetter)
+        .join(' ')
+}
+
 countryNameEl.onkeyup = function (e) {
     if (e.which == 13) {
+        var countryName = capitalizeEachWord(countryNameEl.value);
+        console.log(countryName);
         fetch("/api/visa/?from=" + countryName)
             .then(r => r.json())
-            .then(d => colorCountries(d));
+            .then(dat => colorCountries(dat.d));
     }
 }
