2ea656c262e4ef292e3b0f44397baf08eb4fc6cf
Swaraj Dhumne <swaraj.dhumne@gmail.com>
2017-02-11 21:43:24 -0500

    updated some coloration

--- migrate/static/migrate/mapscript.js
+++ migrate/static/migrate/mapscript.js
@@ -21,9 +21,7 @@ L.Map = L.Map.extend({
     }
 });
 
-fetch("/api")
-    .then(r => r.json())
-    .then(dat => { console.log(dat.d)});
+
 
 var map = L.map('mapid').setView([51.505, -0.09], 4);
 
@@ -39,100 +37,137 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 var lastClickedCountry = null;
 var lastWasBanned = false;
 
-function onEachBannedFeature(feature, layer) {   
-    layer.on('click', function(e) {
-        var popupContent = "";
-        var name = feature.properties.name;
-        if (lastClickedCountry != null && !lastWasBanned) {
-            popupContent = "this country (" + name + ") is banned, last clicked is " + lastClickedCountry;
-        } else {
-            popupContent = "this country (" + name + ") is banned, no last clicked";
+
+fetch("/api")
+    .then(r => r.json())
+    .then(dat => {
+
+        var data = {};
+
+        function onEachBannedFeature(feature, layer) {
+            layer.on('click', function (e) {
+                var popupContent = "";
+                var name = feature.properties.name;
+                if (lastClickedCountry != null && !lastWasBanned) {
+                    popupContent = "this country (" + name + ") is banned, last clicked is " + lastClickedCountry;
+                } else {
+                    popupContent = "this country (" + name + ") is banned, no last clicked";
+                }
+
+                lastWasBanned = true;
+                layer.bindPopup(popupContent).addTo(map).openPopup();
+            })
         }
 
-        lastWasBanned = true;
-        layer.bindPopup(popupContent).addTo(map).openPopup();
-    })
-}
+        function onEachFeature(feature, layer) {
+            layer.on('click', function (e) {
+                lastClickedCountry = feature.properties.name;
+                bannedCountries.eachLayer(function (layer) {
+                    layer.bindPopup(feature.properties.name).addTo(map).openPopup();
+                })
+                lastWasBanned = false;
+            })
+        }
 
-function onEachFeature(feature, layer) {
-    layer.on('click', function (e) {
-        lastClickedCountry = feature.properties.name;
-        bannedCountries.eachLayer(function (layer) {
-            layer.bindPopup(feature.properties.name).addTo(map).openPopup();
-        })
-        lastWasBanned = false;
-    })
-}
+        function isBanned(name) {
+            var bannedCountryList = [
+                "Syria",
+                "Iraq",
+                "Iran",
+                "Sudan",
+                "Yemen",
+                "Somalia",
+                "Libya"
+            ];
+            return bannedCountryList.indexOf(name) > -1;
+        }
 
-function isBanned(name) {
-    var bannedCountryList = [
-        "Syria",
-        "Iraq",
-        "Iran",
-        "Sudan",
-        "Yemen",
-        "Somalia",
-        "Libya"
-    ];
-    return bannedCountryList.indexOf(name) > -1;
-}
+        function bannedStyle(feature) {
+            return {
+                fillColor: '#800026',
+                weight: 2,
+                opacity: 1,
+                color: 'white',
+                dashArray: '3',
+                fillOpacity: 0.3
+            };
+        }
 
-function style(feature) {
-    return {
-        weight: 2,
-        opacity: 0,
-        color: 'white',
-        dashArray: '3',
-        fillOpacity: 0.3
-    };
-}
+        function style(feature) {
+            var styl = {
+                weight: 2,
+                opacity: 1,
+                color: 'white',
+                dashArray: '3',
+                fillOpacity: 0.3
+            };
 
-function bannedStyle(feature) {
-    return {
-        fillColor: '#800026',
-        weight: 2,
-        opacity: 1,
-        color: 'white',
-        dashArray: '3',
-        fillOpacity: 0.3
-    };
-}
+            var fillCol = null;
 
-var countries = L.geoJson(countryData, {
-    style: style,
-    onEachFeature: onEachFeature,
-    pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng, {
-            radius: 8,
-            fillColor: "#ff7800",
-            color: "#000",
-            weight: 1,
-            opacity: 1,
-            fillOpacity: 0.8
+            if (data[feature.properties.name] >= 0.875) {
+                fillCol = '#800026';
+            } else if (data[feature.properties.name] >= 0.75) {
+                fillCol = '#BD0026';
+            } else if (data[feature.properties.name] >= 0.625) {
+                fillCol = '#E31A1C';
+            } else if (data[feature.properties.name] >= 0.5) {
+                fillCol = '#FC4E2A';
+            } else if (data[feature.properties.name] >= 0.375) {
+                fillCol = '#FD8D3C';
+            } else if (data[feature.properties.name] >= 0.25) {
+                fillCol = '#FEB24C';
+            } else if (data[feature.properties.name] >= 0.125) {
+                fillCol = '#FED976';
+            } else if (data[feature.properties.name] >= 0) {
+                fillCol = '#FFEDA0';
+            }
+
+            if (fillCol != null || !isBanned(feature.properties.name)) {
+                styl.fillColor = fillCol;
+            }
+
+            return styl;
+        }
+
+        var data = dat.d;
+
+        var countries = L.geoJson(countryData, {
+            style: style,
+            onEachFeature: onEachFeature,
+            pointToLayer: function (feature, latlng) {
+                return L.circleMarker(latlng, {
+                    radius: 8,
+                    fillColor: "#ff7800",
+                    color: "#000",
+                    weight: 1,
+                    opacity: 1,
+                    fillOpacity: 0.8
+                });
+            }
         });
-    }
-});
 
-var bannedCountries = L.geoJson(countryData, {
-    style: bannedStyle,
-    onEachFeature: onEachBannedFeature,
-    pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng, {
-            radius: 8,
-            fillColor: "#ff7800",
-            color: "#000",
-            weight: 1,
-            opacity: 1,
-            fillOpacity: 0.8
+        var bannedCountries = L.geoJson(countryData, {
+            style: bannedStyle,
+            onEachFeature: onEachBannedFeature,
+            pointToLayer: function (feature, latlng) {
+                return L.circleMarker(latlng, {
+                    radius: 8,
+                    fillColor: "#ff7800",
+                    color: "#000",
+                    weight: 1,
+                    opacity: 1,
+                    fillOpacity: 0.8
+                });
+            },
+            filter: function (feature, layer) {
+                return isBanned(feature.properties.name);
+            }
         });
-    },
-    filter: function (feature, layer) {
-        return isBanned(feature.properties.name);
-    }
-});
 
-countries.addTo(map);
-bannedCountries.addTo(map);
+        countries.addTo(map);
+        bannedCountries.addTo(map);
+
+    });
 
 //click on any other country, show info to banned countries (in those countries)
 //click on one country to another country, show info between the two
